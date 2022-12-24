bebd71ed1fbe8fc3d939b3938e049621d93811be
yongjie <yongjie_zhang_mail@qq.com>
2022-12-24 20:23:40 +0800

    添加 axois 相关代码

--- src/components/MyProduct2.vue
+++ src/components/MyProduct2.vue
@@ -1,8 +1,13 @@
 <template>
 
   <p style="text-align: center">
-    <input type="text" :placeholder="tip"/>
-    <input type="submit">
+    <!--    <p>{{ searchRequest }}</p>-->
+    <input type="text" :placeholder="tip.tip1" v-model="searchRequest.entity.name"/>
+    <input type="text" :placeholder="tip.tip2" v-model="searchRequest.pageSize"/>
+    <input type="text" :placeholder="tip.tip3" v-model="searchRequest.pageNum"/>
+    <button @click="searchPage">搜索</button>
+    <!--    <a href="https://www.baidu.com" @click.prevent="searchPage" >链接</a>-->
+    <!--    <input type="submit" @submit.prevent="searchPage" />-->
   </p>
 
   <button @click="toggleProduct">{{ showProducts ? "隐藏" : "显示" }}商品</button>
@@ -53,36 +58,66 @@ export default {
   ],
   data() {
     return {
-      tip: "请输入查询关键字",
+      tip: {
+        tip1: "请输入查询关键字",
+        tip2: "每页个数",
+        tip3: "第几页",
+      },
       showProducts: true,
       products: [],
       total: 0,
+      searchRequest: {
+        entity: {
+          name: ""
+        },
+        pageNum: "",
+        pageSize: "",
+      },
     }
   },
   methods: {
     toggleProduct() {
       this.showProducts = !this.showProducts;
-    }
+    },
+    axiosPost1() {
+      // console.log("axiosPost1");
+      // axios.post("http://localhost:8080/v1/testos/searchPage", {
+      this.$axios.post("/v1/testos/searchPage", {
+        "entity": {
+          "name": "pytorch"
+        },
+        "pageNum": 1,
+        "pageSize": 10
+      }).then((response) => {
+        // console.log(response.data);
+        this.products = response.data.data.list;
+        this.total = response.data.data.total;
+      }).catch(function (error) {
+
+      }).then(function () {
+
+      });
+    },
+    axiosPost2() {
+      // console.log("axiosPost2");
+      this.$axios({
+        method: "post",
+        url: "/v1/testos/searchPage",
+        data: this.searchRequest,
+      }).then((response) => {
+        // console.log(response.data);
+        this.products = response.data.data.list;
+        this.total = response.data.data.total;
+      }).catch((error) => {
+
+      }).then({})
+    },
+    searchPage() {
+      // console.log(this.searchRequest);
+      this.axiosPost2();
+    },
   },
   created() {
-    // console.log("组件MyProduct2被创建了");
-    // axios.post("http://localhost:8080/v1/testos/searchPage", {
-    this.$axios.post("/v1/testos/searchPage", {
-      "entity": {
-        "name": "pytorch"
-      },
-      "pageNum": 1,
-      "pageSize": 10
-    }).then((response) => {
-      console.log(response.data);
-      this.products = response.data.data.list;
-      this.total = response.data.data.total;
-
-    }).catch(function (error) {
-
-    }).then(function () {
-
-    });
 
   }
 }
